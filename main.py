b6135f4d7f8da6cd7d008169dbdf5d3663717228
lavanya-vanwyk <lavanyavanwyk1@gmail.com>
2025-10-18 13:21:27 +0200

    Cleaned up orginal code and modularised

---
+++ main.py
@@ -0,0 +1,59 @@
+import sys
+import re
+import math
+from naive_bayes import *
+
+#---bayes probability algorithm
+# P(x-word occuring given if in spam or ham) =  no. of emails in testing_set that have x-word + 1 /
+    #                                                     total no. emails in spam/ham + 2
+
+    # the "+1" and "+2" are laplace smoothing for if a probability is zero to avoid errors
+
+def detect_spam(file, prior_probabilities, likelihoods, unique_words, all_words):
+    '''
+    Uses a trained Naive Bayes model to calculate whether a given word is spam in an email
+    based on the probablities calculated in the model. The total probablity of an email being
+    spam is updated with each word checked against the model data.
+    '''
+    with open(file, "r", encoding = "utf-8", errors = "ignore") as c_file:
+        check_file = c_file.read()
+        c_check_words = re.sub(r'[^a-zA-Z0-9\s]', '', check_file)
+        check_list = c_check_words.lower().split()
+
+        spamicity_hamicity = {}
+
+        for category in prior_probabilities:
+            # value to hold spam/hamicity of currrent checked word
+            current_val = prior_probabilities[category]
+            for word in check_list:
+                if word in likelihoods[category]:
+                    current_val += math.log(likelihoods[category][word])
+                else: 
+                  #word not in testing set
+                    new_word_likelihood = 1 / (all_words + len(unique_words))
+                    current_val += math.log(new_word_likelihood)
+
+            spamicity_hamicity[category] = current_val
+
+        if spamicity_hamicity[1] > spamicity_hamicity[0]:
+            print("spam")
+            return "spam"
+        else:
+            print("notspam")
+            return "notspam"
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        sys.exit(1) 
+        
+    file = sys.argv[1]
+    
+    model_params = load_model()
+    if model_params:
+        prior_probabilities, likelihoods, unique_words, all_words = model_params
+    else:
+        # print("Starting training...")
+        prior_probabilities, likelihoods, unique_words, all_words = train_naive_bayes_model()
+        save_model(prior_probabilities, likelihoods, unique_words, all_words)
+        
+    detect_spam(file, prior_probabilities, likelihoods, unique_words, all_words)
---
+++ naive_bayes.py
@@ -0,0 +1,92 @@
+import urllib.request
+import pickle
+import io
+import csv
+import re
+
+model_file = "naive_bayes_model.pkl"
+def train_naive_bayes_model():
+    '''
+    Trains a machine learning model off of a data set of over 5,000 emails.
+    Add the data to a list as tuple along with either a 0 or 1, denoting whether it is spam 
+    or ham.
+    
+    This function returns the prior probabilities for spam and ham, 
+    likelihoods, a list of all the words (non repeating), and the total words across all emails in
+    the data. 
+    
+    '''
+    url = "https://raw.githubusercontent.com/lavanya-vanwyk/spam-detection-data/97e84dfd335eb39ab8a2232ef91b1f3a98f74263/emails.csv"
+    response = urllib.request.urlopen(url)
+    data = response.read()
+    data_as_str = data.decode("utf-8")
+    csv_file = io.StringIO(data_as_str)
+    training_data = csv.reader(csv_file)
+
+    training_set = []
+    counts = {0: 0, 1: 0}
+    total_word_counts = {0: {}, 1: {}}
+    unique_words = set()
+    all_words = 0
+
+    for line in training_data:
+        text = line[0]
+        category = line[1]
+        training_set.append((text, int(category)))
+        
+    for (text, category) in training_set:
+        counts[category] += 1
+        cleaned_email = re.sub(r'[^a-zA-Z0-9\s]', '', text)
+        email_words = cleaned_email.lower().split()
+
+        for w in email_words:
+            unique_words.add(w)
+             # increment count for the number of times it appears
+            total_word_counts[category][w] = total_word_counts[category].get(w, 0) + 1
+
+    all_words += sum(total_word_counts[category].values()) 
+        
+    total_emails = counts[0] + counts [1]
+
+    spam_probability = counts[1] / total_emails # 0.7
+    ham_probability = counts[0] / total_emails # 0.3
+
+    prior_probabilities = {0: ham_probability, 1: spam_probability}
+    likelihoods = {0:{}, 1:{}}
+    
+    for category in total_word_counts:
+        #spam ham RESPECTIVE
+        category_count = sum(total_word_counts[category].values())
+        bayes_denominator = category_count + len(unique_words)
+
+        for w in unique_words:
+           words_in_category = total_word_counts[category].get(w, 0)
+           bayes_numerator = words_in_category + 1
+           likelihoods[category][w] = bayes_numerator / bayes_denominator
+    
+    return prior_probabilities, likelihoods, unique_words, all_words
+
+def save_model(prior_probabilities, likelihoods, unique_words, all_words):
+    '''
+    This function ensures that when the program is first run, the trained model is saved.
+    This avoids any issues with performance in having to retrain the model each time an
+    email needs to be checked.
+    '''
+    model_data = {
+        'prior_probabilities': prior_probabilities,
+        'likelihoods': likelihoods,
+        'unique_words': unique_words,
+        'all_words': all_words
+    }
+    with open(model_file, 'wb') as f:
+        pickle.dump(model_data, f)
+
+def load_model():
+    with open(model_file, 'rb') as f:
+        model_data = pickle.load(f)
+    return (
+        model_data['prior_probabilities'], 
+        model_data['likelihoods'], 
+        model_data['unique_words'], 
+        model_data['all_words']
+    )
